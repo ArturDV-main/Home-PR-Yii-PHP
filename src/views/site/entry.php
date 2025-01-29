c2f802554269379b9adf7e1d9911a9b71a447ae7
leftrana <leftrana@student.21-school.ru>
2025-01-29 14:28:52 +0300

    fix mail name

--- src/views/site/entry.php
+++ src/views/site/entry.php
@@ -1,15 +1,23 @@
 <?php
+
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+// use yii\widgets\ActiveForm;
+use yii\bootstrap5\ActiveForm;
+
 ?>
 <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'name') ?>
 
-    <?= $form->field($model, 'email') ?>
+<div class="col-3"> <!-- 2 из 12, что соответствует 1/6 -->
+    <?= $form->field($model, 'name')->textInput(['class' => 'form-control']) ?>
+</div>
+<div class="col-3"> <!-- 2 из 12, что соответствует 1/6 -->
+    <?= $form->field($model, 'email')->textInput(['class' => 'form-control']) ?>
+</div>
 
-    <div class="form-group">
-        <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary']) ?>
-    </div>
+
+<div class="form-group">
+    <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary']) ?>
+</div>
 
 <?php ActiveForm::end(); ?>
